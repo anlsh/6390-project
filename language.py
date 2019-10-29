0c0ff9c9d82c82dff5584acfa5f00e72a122d7e4
Anish Moorthy <anlsh@protonmail.com>
2019-10-29 01:52:08 -0400

    Big changes, add interpreter and things

---
+++ language.py
@@ -0,0 +1,12 @@
+import operator as op
+
+UNIT = "unit-type-repr"
+NIL = "nil-type-repr"
+
+MACRO_NAMES = ["defun", "deftype", "defvar", "apply", "if", "while", "set", "ref", "dref", "mutref"]
+
+builtin_fns = {
+        '+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv,
+        '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le, '=': op.eq,
+        'print': print
+}
