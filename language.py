3c15ba115e7f9bd140074da1a84638ae488b4f4a
U-NICKSCOMPUTER\Nicholas <nmartucc@u.rochester.edu>
2019-11-11 23:43:32 -0500

    Update builtins

--- language.py
+++ language.py
@@ -1,13 +1,13 @@
 import operator as op
 from dsl_types import FunType, ValType, Tmod
 
-MACRO_NAMES = ["defun", "deftype", "defvar", "apply", "if", "while", "set", "ref", "dref", "mutref"]
+MACRO_NAMES = ["defun", "deftype", "defvar", "apply", "if", "while", "set", "mkref", "setref"]
 bool_map = {"true": True, "false": False}
 
 builtin_fn_vals = {
         '+': op.add, '-': op.sub, '*': op.mul, '/': op.truediv,
         '>': op.gt, '<': op.lt, '>=': op.ge, '<=': op.le,
-        'print': print
+        'not': op.not_
 }
 
 T_NIL = ValType(mod=Tmod.un, tname='nil')
@@ -21,7 +21,11 @@ T_LIN_INT = ValType(mod=Tmod.lin, tname='int')
 builtin_fn_types = {
     "+": FunType(mod=Tmod.un, retT=T_INT, argTs=(T_LIN_INT, T_LIN_INT)),
     "-": FunType(mod=Tmod.un, retT=T_INT, argTs=(T_LIN_INT, T_LIN_INT)),
+    "/": FunType(mod=Tmod.un, retT=T_INT, argTs=(T_LIN_INT, T_LIN_INT)),
+    ">": FunType(mod=Tmod.un, retT=T_BOOL, argTs=(T_LIN_INT, T_LIN_INT)),
     "<": FunType(mod=Tmod.un, retT=T_BOOL, argTs=(T_LIN_INT, T_LIN_INT)),
-    "not": FunType(mod=Tmod.un, retT=T_BOOL, argTs=(T_BOOL,)),
+    ">=": FunType(mod=Tmod.un, retT=T_BOOL, argTs=(T_LIN_INT, T_LIN_INT)),
+    "<=": FunType(mod=Tmod.un, retT=T_BOOL, argTs=(T_LIN_INT, T_LIN_INT)),
+    "not": FunType(mod=Tmod.un, retT=T_BOOL, argTs=(T_LIN_BOOL,)),
 }
 
